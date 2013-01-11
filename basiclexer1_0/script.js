bcdb675721bf6ef67f636918a096efe1217d3510
mhoaglund <maxwell.hoaglund@gmail.com>
2013-01-10 17:25:33 -0800

    Cleaned up codepen export problems.

--- basiclexer1_0/script.js
+++ basiclexer1_0/script.js
@@ -1,20 +1,17 @@
-
+var lexed=new Array;
 $(document).ready(function(){
-    
-    $("#lex").click(function(){
+  
+$("#lex").click(function(){ 
       var origText = $("#orig").val();
       var origPrep = origText.split(" ");
-      
-      for (var i = 0; i < origPrep.length; i++){
-        filterLanguage();
-      }
-      
-      $("#lexed").val(main);
+  
+      origPrep.forEach(function(entry) {
+        
+        if(entry != "Dance"){
+         lexed.push(entry); 
+        }
+      });
+      $("#lexed").val(lexed.join(" "));
     });
 });
 
-function filterLanguage(t) {
-  if(t == "yes"){
-     main.push(t); 
-  }
-}
