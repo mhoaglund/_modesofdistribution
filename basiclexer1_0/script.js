ccf0174d401ac4321f117e88a2ab22208ec65038
mhoaglund <maxwell.hoaglund@gmail.com>
2013-01-12 11:09:18 -0800

    updated lexer to use json "dictionary" instead of a big stack of conditions. this is the last commit message that will not be filtered using the lexer.

--- basiclexer1_0/script.js
+++ basiclexer1_0/script.js
@@ -1,34 +1,29 @@
-
 $(document).ready(function(){
-var lexed=new Array;
-$("#lex").click(function(){ 
-  var lexed = [];
-      var origText = $("#orig").val();
-      var origPrep = origText.split(" ");
-  
-      origPrep.forEach(function(entry) {
-        
-        if(entry == "dance"){
-          entry = "theory";
-        }
-        if(entry == "theory"){
-          entry = "guess";
-        }
-        if(entry == "labor" || entry == "handicap" || entry == "achievement"){
-          entry = "work";
-        }
-        if(entry.length >= 12){
-          entry = "development";
-        }
-        if(entry == "literature"){
-          var lit = entry.split("");
-          entry = "";
-          lexed.push(lit.join(" "));
-        }
-       
-        else lexed.push(entry);
+  var lexed=new Array;
+  var lexicon = {
+    "dance": { "trans":"wetness" },
+    "theory": { "trans":"guess" },
+    "lexicon": { "trans":"box"}
+  }
+
+  $("#lex").click(function(){ 
+    var lexed = [];
+
+        var origText = $("#orig").val();
+        var origPrep = origText.split(" ");
+    
+        origPrep.forEach(function(entry) {
+          try{
+            x=lexicon[entry].trans;
+            entry = x;
+            lexed.push(entry);
+          }
+
+          catch (e){
+            lexed.push(entry);
+          }
+        });
+        $("#lexed").val(lexed.join(" "));
       });
-      $("#lexed").val(lexed.join(" "));
-    });
 });
 
